c453ede2017104d1833e3a63baa4d8a5eb6d4d7d
Aovchinn <demons94@yandex.ru>
2017-08-17 16:16:07 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Component from "inferno-component";
+import "./registerServiceWorker";
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default App;
